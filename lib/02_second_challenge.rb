d5fa53161358e0340dac8c94fe5b51a469eb3eec
Seohee  Im <seoheeim@vt.edu>
2020-07-22 14:27:37 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -3,7 +3,7 @@ def second_challenge
     :montague => {
       :patriarch => {},
       :matriarch => {},
-      :hero => {},
+      :heroine => {},
       :hero_friends => []
       }, 
     :capulet => {
